0ff7bb83ae1a6f4ef787226a3fdabf6035e95266
cstahoviak <carlcstahoviak@gmail.com>
2024-05-08 19:48:47 -0600

    refectoring location of top-level CMakeLists; add a cpp-level CMakeLists for the python bindings; add python bindings for the two_sum algorithm.

---
+++ cpp/bindings/algorithms/two_pointers.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file two_pointers.cpp
+ * @author Carl Stahoviak
+ * @brief Pybind11 bindings for the dsa::algorithms::two_pointers namespace.
+ * @date 2024-05-08
+ */
+
+#include "dsa/algorithms/two_pointers.h"
+
+#include <pybind11/pybind11.h
+
+namespace py = pybind11;
+
+void initTwoPointers(py::module_& m)
+{
+  m.def("two_sum", &dsa::algorithms::two_sum, py::arg("vec", py::arg("target")));
+}
+
+
+
