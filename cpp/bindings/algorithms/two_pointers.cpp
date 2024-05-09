63ad1561beac3fadf2fbecc48acf5ea22c8e68bc
Carl Stahoviak <carlcstahoviak@gmail.com>
2024-05-08 20:22:26 -0600

    Update two_pointers.cpp

--- cpp/bindings/algorithms/two_pointers.cpp
+++ cpp/bindings/algorithms/two_pointers.cpp
@@ -13,7 +13,7 @@ namespace py = pybind11;
 
 void initTwoPointers(py::module_& m)
 {
-  m.def("two_sum", &dsa::algorithms::two_sum, py::arg("vec"), py::arg("target"));
+  m.def("two_sum", &dsa::algorithms::two_pointers::two_sum, py::arg("vec"), py::arg("target"));
 }
 
 
